632eb8546631998708ab72c9d10651d8053d927b
Kai Johnson <kai@awpny.com>
2013-05-29 17:08:11 -0400

    updates to prime and vanilla KJ

--- themes/vanilla/template.php
+++ themes/vanilla/template.php
@@ -13,21 +13,16 @@ require_once(dirname(__FILE__) . '/includes/helpers.inc');
  * Implements hook_preprocess_html().
  */
 function vanilla_preprocess_html(&$vars) {
-    if (drupal_is_front_page()) {
-        _vanilla_add_css('home');
-    }
-    else {
-        _vanilla_add_css('inner');
-    }
-    
     // Add contextual layout(s).
     if ($contexts = context_active_contexts()) {
         foreach ($contexts as $context_name => $context_info) {
-            $added = _vanilla_add_css('contexts/' . $context_name);
+            if (_vanilla_add_css($context_name)) {
+                $added = TRUE;
+            }
         }
     }
-
+    
     if (empty($added)) {
-        _vanilla_add_css('contexts/default');
+        _vanilla_add_css('default');
     }
 }
