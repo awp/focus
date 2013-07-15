1b760bdc0c3610778e6de375c9e6262fe7743fd1
Kai Johnson <kai@awpny.com>
2013-07-15 12:57:27 -0400

    check sites/default folder permissions before continuing update KJ

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,6 +8,8 @@ set -e
 WT4_PATH="`pwd`"
 
 # Check Drush Version
+# Compatible with drush 5.5 and up due to stability to drush make.
+# @see https://drupal.org/node/1719512
 DRUSH=$(`drush --version`)
 DRUSH_MIN="5.5"
 IFS=' ' read -a array <<< $DRUSH
@@ -15,6 +17,13 @@ VERSION=(`echo "${array[2]} < $DRUSH_MIN" | bc`)
 if [ "$VERSION" == "1" ]; then
 	echo "Update requires Drush version $DRUSH_MIN and up; you are running $DRUSH.  Consider updating drush or manually update your project."
 	exit
+fi
+
+# Check that sites/default has proper permissions to be updated
+TEST=$(test -w ../../sites/default; echo $?);
+if [ "$TEST" == "1" ]; then
+    echo "No write permissions on sites/default.  Updating..."
+    sudo chmod -R 755 ../../sites/default
 fi
 
 # Update the profile.
