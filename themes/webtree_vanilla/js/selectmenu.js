f38c4b7ba49ae0165788371829394c522ed6b406
Kai Johnson <kai@awpny.com>
2013-07-17 17:09:20 -0400

    update webtree_vanilla base scripts KJ

--- themes/webtree_vanilla/js/selectmenu.js
+++ themes/webtree_vanilla/js/selectmenu.js
@@ -1,55 +1,22 @@
-(function($){
+(function($) {
 
-    var webTreeFormsInit = function(cx, s) {
-
-        var widgetTypes = s.fieldWidgetTypes;
-
-        // initialize selectmenus
-        if (!!$.ui.selectmenu) {
-            $('select:visible').not('[multiple]').selectmenu();
-
-            // collapsed fieldsets need manual handling for child select objects
-            $('fieldset.collapsed, fieldset.field-group-tab').each(function(i, el){
-                var fs = this;
-                $('legend a', fs).click(function(){
-                    $('select:visible', fs).selectmenu();
+    Drupal.behaviors.webtreeVanillaSelectmenu = {
+        attach: function(cx, s) {
+            if (!!$.ui.selectmenu) {
+                // initialize selectmenus
+                $('select:visible', cx).not('[multiple]').each(function(i, el) {
+                    var parent = $(el).parent();
+                    $(el).selectmenu({ style: 'popup', appendTo: parent });
                 });
+            }
 
-                $('.vertical-tab-button a').click(function(){
-                    $('select:visible', fs).selectmenu();
+            if (!!$.ui.button) {
+                // initialize checkboxes
+                $('[type="checkbox"]', cx).each(function(i, el){
+                    $(el).button();
                 });
-            });
-
-            // hide/show weight fields need manual handling for select objects
-            $('form').each(function(i, el){
-                var form = this;
-                $('.tabledrag-toggle-weight', this).click(function(){
-                    $('select:visible', form).selectmenu();
-                });
-            });
-
-            // trigger re-build of the selectmenu if there is a dependent select box
-            $('select').change(function(e){
-                $(this.targetSelect).selectmenu('destroy').selectmenu();
-            });
+            }
         }
-
-        // initialize checkboxes
-        // if (!!$.ui.button) $('[type="checkbox"], [type="radio"]').each(function(i, el) {
-            // if (!$(el).siblings('label').is('.element-invisible')) {
-                // $(el).button();
-                    // .bind('change state', function(e){
-                        // setTimeout(function(){ // needs this delay to not beat the original change
-                            // $(e.target).button('refresh');
-                        // }, 1);
-                    // });
-            // }
-        // });
-
-    }
-
-    Drupal.behaviors.webtreeForms = {
-        attach: webTreeFormsInit
     }
 
 })(jQuery);
