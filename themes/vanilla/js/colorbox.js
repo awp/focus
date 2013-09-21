3ee9d9da1f3ce1d400b2ba8ca534f80c12671ec6
Kai Johnson <kaidjohnson@gmail.com>
2013-09-21 15:12:29 -0400

    move colorbox-open up one level to the html element KJ

--- themes/vanilla/js/colorbox.js
+++ themes/vanilla/js/colorbox.js
@@ -5,10 +5,10 @@
             $('body').once('vanilla-colorbox', function() {
                 $(window).bind({
                     cbox_load: function() {
-                        $('body').addClass('colorbox-open');
+                        $('html').addClass('colorbox-open');
                     },
                     cbox_cleanup: function() {
-                        $('body').removeClass('colorbox-open');
+                        $('html').removeClass('colorbox-open');
                     }
                 });
             });
@@ -18,7 +18,7 @@
     if (!!Drupal.settings.colorbox) {
         Drupal.settings.colorbox.title = function() {
             var title = $(this).attr('title');
-            var html  = '<table cellspacing="0"><tr><td>'+title+'</td></tr></table>'; 
+            var html  = '<table cellspacing="0"><tr><td>'+title+'</td></tr></table>';
             return (!title) ? false : html;
         }
     }
